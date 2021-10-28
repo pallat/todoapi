c60ae251d9c589513646f5d5570461b08e2f2331
Pallat Anchaleechamaikorn <yod.pallat@gmail.com>
2021-10-28 22:12:57 +0700

    gorm example

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/pallat/todoapi
+
+go 1.17
+
+require (
+	gorm.io/driver/sqlite v1.2.3
+	gorm.io/gorm v1.22.2
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.9 // indirect
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name string
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+
+	db.Create(&User{Name: "Pallat"})
+
+	var u User
+	db.First(&u)
+
+	fmt.Println(u.ID, u.Name)
+}
